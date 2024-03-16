555489177af657618b287bbc2367baf4c040ba14
michellimadesigner <michellimadesigner@outlook.com>
2024-03-15 23:12:18 -0300

    wip: created a section to create a new post in feed

--- src/presentation/pages/Feed.tsx
+++ src/presentation/pages/Feed.tsx
@@ -1,9 +1,44 @@
-import FeedContainer from "../containers/feed/Feed.container"
+import { Picture } from '@icon-park/react'
+import FeedContainer from '../containers/feed/Feed.container'
+import { Modal, ModalBody, ModalContent, ModalHeader, useDisclosure } from '@nextui-org/react'
+import PostContainer from '../containers/post/Posts.container'
 
 const Feed: React.FC = () => {
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+
+  const closeModal = () => {
+    onOpenChange()
+  }
+
   return (
-    <div className='w-full'>
+    <div className='w-full pt-16'>
+      <div
+        className='flex gap-4 mx-auto w-full max-w-[700px] border-2 border-red-400 text-primary border-dashed rounded-2xl p-8 items-center justify-center cursor-pointer'
+        onClick={onOpen}
+      >
+        <Picture theme='outline' size='24' fill='#e03f5c' strokeWidth={3}/>
+        <p>Criar nova publicação</p>
+      </div>
+
       <FeedContainer />
+
+      <Modal
+        backdrop='blur' 
+        isOpen={isOpen} 
+        onOpenChange={onOpenChange}
+        size='4xl'
+      >
+        <ModalContent>
+          {() => (
+            <>
+              <ModalHeader className='flex flex-col gap-1'>Criar nova publicação</ModalHeader>
+              <ModalBody>
+                <PostContainer closeModal={closeModal}/>
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </div>
   )
 }
