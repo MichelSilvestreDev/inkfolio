cd4dd1b283f16e825c54d6ccbc6ef2563db8b352
michellimadesigner <michellimadesigner@outlook.com>
2024-04-16 23:34:18 -0300

    wip:Created the Home Header Component

---
+++ src/presentation/components/home/HomeHeader.tsx
@@ -0,0 +1,35 @@
+import { Input } from '@nextui-org/react'
+import InkFolioLogo from '/logos/InkFolio-white.png'
+import { Search } from '@icon-park/react'
+
+const HomeHeader: React.FC = () => {
+  return (
+    <div className='w-full pt-8 pb-16 relative'>
+      <div className='container'>
+        <div className='flex flex-col justify-center items-center'>
+          <img
+            src={InkFolioLogo} 
+            alt='InkFolio'
+            className='w-40 pb-8'
+          />
+
+          <Input
+            label='Pesquisar'
+            className=''
+            endContent={
+              <Search
+                theme='outline'
+                size='24'
+                fill='#333'
+                strokeWidth={3}
+              />
+            }
+          />
+        </div>
+      </div>
+      <div className='bg-black absolute h-64 w-full top-0 left-0 -z-10'></div>
+    </div>
+  )
+}
+
+export default HomeHeader
--- src/presentation/pages/Home.tsx
+++ src/presentation/pages/Home.tsx
@@ -1,21 +1,10 @@
-import { Input } from '@nextui-org/react'
-import InkFolioLogo from '/logos/InkFolio-white.png'
+import HomeHeader from "../components/home/HomeHeader"
+
 
 const Home: React.FC = () => {
   return (
     <div className='w-full'>
-      <div className='w-full bg-black flex justify-center items-center py-8'>
-        <img
-          src={InkFolioLogo} 
-          alt='InkFolio'
-          className='w-40 py-4'
-        />
-
-        <Input
-          label='Pesquisar'
-          className='hidden'
-        />
-      </div>
+      <HomeHeader />
     </div>
   )
 }
