7ff02eca81127159b7d86707fce91622aaa0d8f8
michellimadesigner <michellimadesigner@outlook.com>
2024-04-22 23:20:13 -0300

    feat: add Loader and LoaderPage components

---
+++ src/presentation/pages/LoaderPage.tsx
@@ -0,0 +1,11 @@
+import Loader from "../../common/Loader"
+
+const LoaderPage: React.FC = () => {
+  return (
+    <div className='w-scree h-screen flex justify-center items-center'>
+      <Loader />
+    </div>
+  )
+}
+
+export default LoaderPage
