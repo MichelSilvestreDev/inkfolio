dd830f31c5ba47658e385eea3c68d6df42787158
michellimadesigner <michellimadesigner@outlook.com>
2024-05-03 15:25:29 -0300

    style: adjust new account button responsive

--- src/common/NewAccountFloatBtn.tsx
+++ src/common/NewAccountFloatBtn.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 export const NewAccountFloatBtn: React.FC = () => {
   return(
-    <div className='fixed bottom-8 right-4 z-50'>
+    <div className='fixed bottom-4 sm:bottom-8 right-4 z-50'>
       <Link to='/cadastro' target='_blank'>
         <Button size='sm' color='primary' radius='lg' className='shadow-lg'>
           <Plus theme="outline" size="24" fill="#fff" strokeWidth={3}/>
