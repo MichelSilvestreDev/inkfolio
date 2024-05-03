e12386e0a21bcc187fc9de74c04ae54a358df208
michellimadesigner <michellimadesigner@outlook.com>
2024-05-03 14:56:54 -0300

    feat: add create account float button

---
+++ src/common/NewAccountFloatBtn.tsx
@@ -0,0 +1,16 @@
+import { Plus } from '@icon-park/react'
+import { Button } from '@nextui-org/react'
+import { Link } from 'react-router-dom'
+
+export const NewAccountFloatBtn: React.FC = () => {
+  return(
+    <div className='fixed bottom-8 right-4 z-50'>
+      <Link to='/cadastro' target='_blank'>
+        <Button size='sm' color='primary' radius='lg' className='shadow-lg'>
+          <Plus theme="outline" size="24" fill="#fff" strokeWidth={3}/>
+          Crie o seu InkFolio
+        </Button>
+      </Link>
+    </div>
+  )
+}
