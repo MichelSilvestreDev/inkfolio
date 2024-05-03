ea694aa6934114d450c7329c2acb6f02cda01eec
Pigor66 <higorribeirosantos@gmail.com>
2024-05-03 14:57:12 -0300

    feature: ajust image and infos from postCard

--- src/presentation/components/feed/PostCard.tsx
+++ src/presentation/components/feed/PostCard.tsx
@@ -23,7 +23,7 @@ const PostCard: React.FC<Card> = ({ post }: Card) => {
   }
 
   return (
-    <Card className='py-4 max-w-[700px] min-h-[500px] shadow-none mx-auto my-12 bg-transparent overflow-visible'>
+    <Card className='py-4 max-w-[500px] min-h-[650px] shadow-none mx-auto my-12 bg-transparent overflow-visible'>
       <CardHeader className='pb-0 pt-2 px-0 justify-between mb-4'>
         <Link to={`/perfil/${post.user.profileUrl}`}>
           <User
@@ -35,25 +35,27 @@ const PostCard: React.FC<Card> = ({ post }: Card) => {
           />
         </Link>
       </CardHeader>
-      <CardBody className='relative overflow-visible px-0 pb-0 mb-0'>
+      <CardBody className='relative overflow-visible px-0 '>
         <PostImgSlide urls={post.urls} />
         <div
-          className="w-full h-16 z-20 px-4 rounded-b-xl flex absolute bottom-0 bg-black/40 border-t-1 border-default-600 dark:border-default-100"
+          className="w-full h-18 z-20 px-4 py-3 rounded-b-lg flex absolute bottom-5 bg-white	  "
         >
           <div className="w-full flex flex-grow gap-2 items-center">
             <div className="flex flex-col">
-              <h3 className="text-white/60"><strong>{post.title}</strong></h3>
-              <p className="text-tiny text-white/60">{post.description}</p>
+              <h3 ><strong>{post.title}</strong></h3>
+              <p className="text-tiny ">{post.description}</p>
               {
                 post.price && (
-                  <p className="text-tiny text-white/60 text-primary">{convertToBRACurrency(post.price)}</p>
+                  <p className="text-tiny  text-primary">{convertToBRACurrency(post.price)}</p>
                 )
               }
             </div>
           </div>
-          <Tooltip content='Em breve'>
-            <Button onClick={shareOnWhatsApp} radius="full" size="sm" className='mt-4 min-w-16'>Pedir orçamento</Button>
+         <div>
+         <Tooltip content='Em breve' >
+            <Button onClick={shareOnWhatsApp} radius="full" size="sm" className='mt-2 min-w-16 px-8'>Pedir orçamento</Button>
           </Tooltip>
+         </div>
         </div>
       </CardBody>
       <CardFooter className="flex gap-4 pt-8 px-0 relative z-20 hidden">
