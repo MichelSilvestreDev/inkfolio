eb58edbb72888038e077b25856011f5ffa16695f
michellimadesigner <michellimadesigner@outlook.com>
2024-05-04 11:09:31 -0300

    fix: bug in build app

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  base: '/',
-  build: {
-    outDir: 'build', // Diretório de saída do build
-    assetsDir: 'assets', // Diretório de assets
-    emptyOutDir: true, // Limpar o diretório de saída antes de cada construção
-    sourcemap: true, // Gerar mapas de origem
-  },
-  server: {
-    port: 3000, // Porta do servidor de desenvolvimento
-    open: true, // Abra o navegador ao iniciar o servidor de desenvolvimento
-    proxy: {
-      // Configuração de proxy, se necessário
-    },
-  },
-});
+  plugins: [react()],
+  // base: '/',
+  // build: {
+  //   outDir: 'build', // Diretório de saída do build
+  //   assetsDir: 'assets', // Diretório de assets
+  //   emptyOutDir: true, // Limpar o diretório de saída antes de cada construção
+  //   sourcemap: true, // Gerar mapas de origem
+  // },
+  // server: {
+  //   port: 3000, // Porta do servidor de desenvolvimento
+  //   open: true, // Abra o navegador ao iniciar o servidor de desenvolvimento
+  //   proxy: {
+  //     // Configuração de proxy, se necessário
+  //   },
+  // },
+})
