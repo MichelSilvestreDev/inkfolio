ef0ab874d3dcf51f7202c61faf67d7975e7bea64
André Fernandes <andre@celebrar.com.br>
2024-05-03 23:24:59 -0300

    fix: general fixes and test for error at production url

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [reactRefresh()],
+  build: {
+    outDir: 'build', // Diretório de saída do build
+    assetsDir: 'assets', // Diretório de assets
+    emptyOutDir: true, // Limpar o diretório de saída antes de cada construção
+    sourcemap: true, // Gerar mapas de origem
+  },
+  server: {
+    port: 3000, // Porta do servidor de desenvolvimento
+    open: true, // Abra o navegador ao iniciar o servidor de desenvolvimento
+    proxy: {
+      // Configuração de proxy, se necessário
+    },
+  },
+});
